aa35539bc3280e466310c6ac016472caf60f6879
Abdulvokhid <mr.ochilov1997@gmail.com>
2021-10-20 02:05:09 +0900

    remove console.log

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -10,7 +10,6 @@ const MealItemForm = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
 
-    console.log(amountInputRef);
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
 
